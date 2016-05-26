88345c6cd8d622674081fcfd09b5d60b65edae81
Hendrik Werner <hendrik.to@gmail.com>
2016-05-26 19:14:13 +0200

    Add handler stubs for the buttons.

--- assignment14_2/FXMLDocumentController.java
+++ assignment14_2/FXMLDocumentController.java
@@ -2,6 +2,8 @@ package assignment14_2;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -13,6 +15,22 @@ public class FXMLDocumentController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    }
+
+    /**
+     * @param e the action event
+     */
+    @FXML
+    private void handleStartButton(ActionEvent e) {
+        System.out.println("start");
+    }
+
+    /**
+     * @param e the action event
+     */
+    @FXML
+    private void handleStopButton(ActionEvent e) {
+        System.out.println("stop");
     }
 
 }
