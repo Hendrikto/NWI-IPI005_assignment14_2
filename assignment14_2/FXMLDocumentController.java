0294ed329c13a5da5e8f7a4e03ffce93d58bb941
Hendrik Werner <hendrik.to@gmail.com>
2016-05-27 20:04:26 +0200

    Bind the input to the generator.

--- assignment14_2/FXMLDocumentController.java
+++ assignment14_2/FXMLDocumentController.java
@@ -3,11 +3,14 @@ package assignment14_2;
 import ackermann.Ackermann;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.util.StringConverter;
+import javafx.util.converter.NumberStringConverter;
 
 /**
  *
@@ -24,6 +27,9 @@ public class FXMLDocumentController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         generator = new Ackermann();
+        StringConverter<Number> converter = new NumberStringConverter();
+        Bindings.bindBidirectional(inputM.textProperty(), generator.mProperty(), converter);
+        Bindings.bindBidirectional(inputN.textProperty(), generator.nProperty(), converter);
     }
 
     /**
